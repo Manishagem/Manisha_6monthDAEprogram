09bcb54de63e2d3f8ce10ace0b140661170928b0
Manishagem <manishagupta035@gmail.com>
2026-02-25 14:34:48 -0500

    Add code to GoFit.py

--- pyhton_1/GoFit.py
+++ pyhton_1/GoFit.py
@@ -5,22 +5,41 @@
 
 # Function prints Gain weight workout upon call, accepts age as argument.
 def GainWorkout(GetAge):
+    # Dictionary that holds data for workout plan.
+    GainWorkout_plan = {
+                50 : {"Weekly structure":["5 Training Days", "2 Recovery Days"],"Cardio Type":["Light walking"],"Key Exercises":["Bench Press","Squats","Deadlifts","Pull-ups","Barbell Curls"],"Duration":"45–70 minutes","Calorie Recommendation":"300–400 calories above maintenance","Protein Guidelines":"0.7–1g per lb bodyweight","Expected Weekly Weight Gain":"0.25–0.375 lbs"}, 
+                70  : {"Weekly structure":["4 Strength Days", "2 Light Activity Days", "1 Full Rest Day"],"Cardio Type":["30 min Brisk walking", "light cardio 15 -20 mins"],"Key Exercises":["Dumbbell Chest Press","Goblet Squats","Lat Pulldown","Romanian Deadlifts","Plank"],"Duration":"45–60 minutes","Calorie Recommendation":"250–350 calories above maintenance","Protein Guidelines":"0.8–1g per lb bodyweight","Expected Weekly Weight Gain":"0.125–0.25 lbs"}, 
+                100 : {"Weekly structure":["3 Strength Days", "2 Light Movement Days", "2 Full Rest Days"],"Cardio Type":["20–30 minute brisk walk or cycling"],"Key Exercises":["Chair Squats","Wall Push-ups","Seated Dumbbell Shoulder Press","Resistance Band Rows"],"Duration":"30–45 minutes","Calorie Recommendation":"Small surplus (Eat slightly more than usual)","Protein Guidelines":"Protein at every meal (eggs, fish, chicken, lentils, Greek yogurt)","Expected Weekly Weight Gain":"Less than 0.125 lbs "}} 
+    TempAge=1
     if (GetAge>17 and GetAge<51):
-        print( "\nGet workout plan1\n" )
+        TempAge = 50
     elif (GetAge>50 and GetAge<71):
-        print( "\nGet workout plan2\n" )
+        TempAge = 70
     elif (GetAge>70 and GetAge<100):
-        print( "Get workout plan3" )  
+        TempAge = 100
+    print( "\n Please find below your 7 day workout plan to gain weight. All the best! \n" )    
+    print( "\n","Weekly Structure: ",GainWorkout_plan[TempAge]["Weekly structure"],"\n\n","Cardio Type: " ,GainWorkout_plan[TempAge]["Cardio Type"],"\n\n","Key Exercises: ", GainWorkout_plan[TempAge]["Key Exercises"],"\n\n","Duration: ", GainWorkout_plan[TempAge]["Duration"],"\n\n","Calorie Recommendation: ", GainWorkout_plan[TempAge]["Calorie Recommendation"],"\n\n","Protein Guidelines: ", GainWorkout_plan[TempAge]["Protein Guidelines"],"\n\n","Expected Weekly Weight Gain: ", GainWorkout_plan[TempAge]["Expected Weekly Weight Gain"],"\n"  )     
 
 # Function prints Loose weight workout upon call, accepts age as argument.
 
 def LooseWorkout(GetAge):
+    # Dictionary that holds data for workout plan.
+    LooseWorkout_plan = {
+                50 : {"Weekly structure":["4 Strength Days", "2 Cardio Days", "1 Active Recovery Day"],"Cardio Type":["Brisk walking", "jogging", "cycling", "elliptical", "HIIT(Jump squats, mountain climbers, burpees)"],"Key Exercises":["Squats","Push-ups","Dumbbell Rows","Lunges","Bench Press","Romanian Deadlifts"],"Duration":"45–60 minutes","Calorie Deficit Recommendation":"300–500 calories below maintenance","Protein Guidelines":"0.7–1g per lb bodyweight","Expected Weekly Weight Loss":"0.5–1 lbs"}, 
+                70  : {"Weekly structure":["3 Strength Days", "2 Cardio Days", "1 Active Recovery Day","1 Full Rest Day"],"Cardio Type":["Brisk walking", "cycling", "elliptical", "swimming"],"Key Exercises":["Goblet Squats","Dumbbell Chest Press","Seated Rows","Leg Press","Bench Press","Romanian Deadlift"],"Duration":"40–60 minutes","Calorie Deficit Recommendation":"300–400 calories below maintenance","Protein Guidelines":"Protein at every meal","Expected Weekly Weight Loss":"0.5–1 lbs"}, 
+                100 : {"Weekly structure":["3 Strength/Resistance Days", "2 Cardio Days (low impact)", "1  Mobility / Balance Day","1 Full Rest Day"],"Cardio Type":["Walking (flat terrain)", "stationary bike", "elliptical", "swimming"],"Key Exercises":["Chair Squats","Wall Push-ups","Resistance Band Rows","Step-ups","Bird Dog"],"Duration":"30–50 minutes","Calorie Deficit Recommendation":"250–300 calories below maintenance","Protein Guidelines":"25–30g per meal","Expected Weekly Weight Loss":"0.25–0.5 lbs"}} 
+    TempAge=1
+
     if (GetAge>17 and GetAge<51):
-        print( "Get workout plan1" )
+        TempAge = 50
     elif (GetAge>50 and GetAge<71):
-        print( "Get workout plan2" )
+        TempAge = 70
     elif (GetAge>70 and GetAge<100):
-        print( "Get workout plan3" )  
+        TempAge = 100 
+
+    print( "\n Please find below your 7 day workout plan to loose weight. All the best! \n" )    
+    print( "\n","Weekly Structure: ",LooseWorkout_plan[TempAge]["Weekly structure"],"\n\n","Cardio Type: " ,LooseWorkout_plan[TempAge]["Cardio Type"],"\n\n","Key Exercises: ", LooseWorkout_plan[TempAge]["Key Exercises"],"\n\n","Duration: ", LooseWorkout_plan[TempAge]["Duration"],"\n\n","Calorie Deficit Recommendation: ", LooseWorkout_plan[TempAge]["Calorie Deficit Recommendation"],"\n\n","Protein Guidelines: ", LooseWorkout_plan[TempAge]["Protein Guidelines"],"\n\n","Expected Weekly Weight Loss: ", LooseWorkout_plan[TempAge]["Expected Weekly Weight Loss"],"\n"  )         
+  
    
 # Program execution starts here, Prints a welcome message.
 print( "\n Welcome to GoFit App! \n GoFit generates a 7 day workout plan for you, either to gain or loose weight." )
